2a0c536fa3cbdd954941eb9a6e18d45250bdd9a8
CMSTie <cmstie@gmail.com>
2020-07-21 17:15:13 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -6,7 +6,17 @@ class PokemonCollection extends React.Component {
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        
+        {this.props.pokemons.map(pokemon =>
+          <PokemonCard
+            key={pokemon.id}
+            id={pokemon.id}
+            name={pokemon.name}
+            hp={pokemon.hp}
+            frontImg={pokemon.sprites.front}
+            backImg={pokemon.sprites.back}
+          />
+          )}
       </Card.Group>
     )
   }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,44 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemons: [],
+    search: ''
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      pokemons: data
+    }))
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleNewPokemon = (newPokemon) => {
+    this.setState({pokemons: [...this.state.pokemons, newPokemon]})
+  }
+
   render() {
+    console.log(this.state);
+    let filteredPokemon = this.state.pokemons.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(this.state.search.toLowerCase())
+      )
+      
+
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleNewPokemon={this.handleNewPokemon}/>
         <br />
-        <Search />
+        <Search handleChange={this.handleChange} search={this.state.search}/>
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={filteredPokemon}/>
       </Container>
     )
   }
