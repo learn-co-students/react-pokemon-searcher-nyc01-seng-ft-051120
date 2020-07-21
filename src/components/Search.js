79ca05087a747bc9624a0b59d3b0e64236ceaa71
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-07-21 12:11:58 -0400

    Done.

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ const Search = props => {
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt"/>
+        <input onChange={props.handleSearch} className="prompt" value={props.search}/>
         <i className="search icon" />
       </div>
     </div>
